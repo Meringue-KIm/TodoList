d37f7669d9ea6b4a7a768ffcbe8813a276b5abfc
sungjun kim <dd8873@naver.com>
2022-09-14 22:07:12 +0900

    CommentEntity

---
+++ src/main/java/com/study/TodoList/entity/Comment.java
@@ -0,0 +1,26 @@
+package com.study.TodoList.entity;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Comment {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private LocalDate commentDate;
+
+    @ManyToOne
+    @JoinColumn(name = "tdl_id")//??
+    private Tdl tdl;
+
+}
