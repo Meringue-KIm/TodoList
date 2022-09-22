c413fd33e1825e99bb1e245ee112aacdb12a9d67
sungjun kim <dd8873@naver.com>
2022-09-22 17:24:16 +0900

    ..

--- src/main/java/com/study/TodoList/controller/TdlController.java
+++ src/main/java/com/study/TodoList/controller/TdlController.java
@@ -5,11 +5,9 @@ import com.study.TodoList.service.TdlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class TdlController {
 
     @Autowired
